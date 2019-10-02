d47074426d116d18a359967b442d4b71082acea1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-02 20:15:12 +0000

    Deploy 0xADB/allocator to github.com/0xADB/allocator.git:gh-pages

--- search/all_7.js
+++ search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['get_5fallocator',['get_allocator',['../classnonstd_1_1list.html#ab280fa705923608826d878ba921ebb02',1,'nonstd::list']]],
   ['get_5fend_5fslot',['get_end_slot',['../structnonstd_1_1list__details_1_1header.html#ac5c60e6dbf606141eec58adddc3215b9',1,'nonstd::list_details::header::get_end_slot() const '],['../structnonstd_1_1list__details_1_1header.html#a69ff4f156adedcfa2fce413dd6656f41',1,'nonstd::list_details::header::get_end_slot()']]],
   ['get_5flast_5fnode_5fslot',['get_last_node_slot',['../structnonstd_1_1list__details_1_1header.html#aa551903ab2e9b8f598e3fe3693b44020',1,'nonstd::list_details::header::get_last_node_slot() const '],['../structnonstd_1_1list__details_1_1header.html#a531471e88d0582bedc01dd9a5f271b4f',1,'nonstd::list_details::header::get_last_node_slot()']]]
 ];
